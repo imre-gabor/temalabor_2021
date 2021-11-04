f156a59dac74be75869891b56edea702f6723ccf
imre-gabor <gabor@aut.bme.hu>
2021-11-04 17:12:27 +0100

    transform Product ant Category into JPA entities

--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/model/Category.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/model/Category.java
@@ -3,16 +3,25 @@ package hu.bme.aut.temalabor.webshop.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue
     private long id;
     private String name;
     
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
     
     
--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/model/Product.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/model/Product.java
@@ -1,15 +1,24 @@
 package hu.bme.aut.temalabor.webshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private double price;
     
+    @ManyToOne
     private Category category;
 }
