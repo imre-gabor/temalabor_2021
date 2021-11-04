c72c5f301d474376d8e672ceb570d7315bba3a12
imre-gabor <gabor@aut.bme.hu>
2021-11-04 17:26:18 +0100

    add CommandLineRunner

--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/WebshopApplication.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/WebshopApplication.java
@@ -1,12 +1,24 @@
 package hu.bme.aut.temalabor.webshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import hu.bme.aut.temalabor.webshop.service.SampleDataService;
+
 @SpringBootApplication
-public class WebshopApplication {
+public class WebshopApplication implements CommandLineRunner {
+    
+    @Autowired
+    SampleDataService sampleDataService;
 
     public static void main(String[] args) {
         SpringApplication.run(WebshopApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        sampleDataService.addSampleData();
     }
 }
