657faf637cae934ddbfb594761eacbb4bfbf4704
imre-gabor <gabor@aut.bme.hu>
2021-11-04 17:15:49 +0100

    add spring annotations to service class

--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/service/DiscountService.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/service/DiscountService.java
@@ -2,13 +2,20 @@ package hu.bme.aut.temalabor.webshop.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import hu.bme.aut.temalabor.webshop.model.Category;
 import hu.bme.aut.temalabor.webshop.repository.CategoryRepository;
 import hu.bme.aut.temalabor.webshop.repository.ProductRepository;
 
+@Service
 public class DiscountService {
     
+    @Autowired
     private ProductRepository productRepository;
+    
+    @Autowired
     private CategoryRepository categoryRepository;
     
     public void discountProductsInCategory(String name, int percent) {
