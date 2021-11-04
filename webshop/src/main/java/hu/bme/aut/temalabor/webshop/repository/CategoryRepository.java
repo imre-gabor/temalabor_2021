a57700c0cb07e1e5bd49c36c8c7642fc345d01b1
imre-gabor <gabor@aut.bme.hu>
2021-11-04 17:14:45 +0100

    extend repositoried from JpaRepository

--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/repository/CategoryRepository.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package hu.bme.aut.temalabor.webshop.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import hu.bme.aut.temalabor.webshop.model.Category;
 
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     public List<Category> findByName(String name);
     
--- webshop/src/main/java/hu/bme/aut/temalabor/webshop/repository/ProductRepository.java
+++ webshop/src/main/java/hu/bme/aut/temalabor/webshop/repository/ProductRepository.java
@@ -2,13 +2,12 @@ package hu.bme.aut.temalabor.webshop.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import hu.bme.aut.temalabor.webshop.model.Product;
 
-public interface ProductRepository {
-
-    public Product save(Product product);
+public interface ProductRepository extends JpaRepository<Product, Long>{
     
     public List<Product> findByName(String name);
     
-    public void delete(Product product);
 }
